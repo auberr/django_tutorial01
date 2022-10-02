dc6f21acf7cb5a973d91deefff00b1af4b75c952
auberr <auberr@daum.net>
2022-10-02 18:11:06 +0900

    updated

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,16 @@
+from uuid import uuid4
+
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Feed
+import os
+from config.settings import MEDIA_ROOT
 
 # Create your views here.
 class Main(APIView):
     def get(self, reuqest):
-        feed_list = Feed.objects.all()
+        feed_list = Feed.objects.all().order_by('-id')
         
         for feed in feed_list:
             print(feed.content)
@@ -16,4 +21,26 @@ class Main(APIView):
         print("포스트로 호출")
         return render(request, "main.html")
 
-    
+
+class UploadFeed(APIView):
+    def post(self, request):
+
+        # 일단 파일 불러와
+        file = request.FILES['file']
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        image= uuid_name
+        content = request.data.get('content')
+        user_id = request.data.get('user_id')
+        profile_image= request.data.get('profile_image')
+
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+
+        return Response(status=200)
+
--- user/models.py
+++ user/models.py
@@ -1,5 +1,21 @@
 from django.db import models
-# from django.contrib.auth.user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser
 
 # Create your models here.
-# class 
+class User(AbstractBaseUser):
+    '''
+        유저 프로파일 사진
+        유저 아이디      -> 화면에 표기되는 이름
+        유저 이메일 주소 -> 회원가입 떄 사용하는 아이디
+        유저 비밀번호    -> 디폴트 쓰자
+    '''
+
+    profile_img = models.TextField() # 프로필 이미지
+    nickname = models.CharField(max_length=24, unique=True)
+    name = models.CharField(max_length=24)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'nickname'
+
+    class Meta:
+        db_table = "User"
